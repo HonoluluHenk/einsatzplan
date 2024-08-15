cfdd8e765c0cd31093162143c2444518eb24d41b
Christoph Linder <github@christoph-linder.ch>
2024-08-15 18:51:38 +0200

    delete unused writeJsonToFile.ts

--- apps/data-scraper/src/writeJsonToFile.ts
+++
@@ -1,5 +0,0 @@
-import * as  fs from 'fs/promises';
-
-export async function writeJsonToFile(value: unknown, filePath: string) {
-  return await fs.writeFile(filePath, JSON.stringify(value, null, 2) + '\n');
-}
