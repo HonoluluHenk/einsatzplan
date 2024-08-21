52ef3f4ab4357f5a16c72b21f27a4d8caa1d1519
Christoph Linder <github@christoph-linder.ch>
2024-08-22 00:17:56 +0200

    debug nx-plugin:test in github actions

--- tools/nx-plugin/jest.config.ts
+++ tools/nx-plugin/jest.config.ts
@@ -2,8 +2,9 @@
 export default {
   displayName: 'nx-plugin',
   preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^.+\\.[tj]s$': ['ts-jest', {tsconfig: '<rootDir>/tsconfig.spec.json'}],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/tools/nx-plugin',
